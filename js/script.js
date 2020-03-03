9fbe63f037b95e7a0b3654fcd527d396aa1e21f3
Martha Hipley <info@marthahipley.com>
2020-03-03 14:43:56 -0600

    putting a pin in fill patterns and clearing out old functions

--- js/script.js
+++ js/script.js
@@ -91,6 +91,7 @@ rasterInit.on('load', function() {
       edge.add(event.point);
       // edge.strokeColor = 'white';
       edge.fillColor = 'white';
+
       
   }
 
@@ -113,6 +114,8 @@ rasterInit.on('load', function() {
 
 
       var childIndex = edge.parent.lastChild.index;
+
+
 
 
       // edge.deselectAll;
@@ -182,8 +185,6 @@ rasterInit.on('load', function() {
       var textures = new Group();
 
       var texturesNo = Math.round(path.length / 40);
-
-      console.log(texturesNo);     
 
       // function drawSquares(path, texturesNo) {
 
@@ -282,88 +283,6 @@ rasterInit.on('load', function() {
   }
 
 });
-
-var actionPath;
-// var result;
-
-// function bisectBounding(boundingBox) {
-//   var boundB = boundingBox.clone();
-//   boundB.style = {
-//     strokeColor: "blue",
-//   }
-// }
-
-
-
-// var circle = new Path.Circle(new Point(80, 50), 200);
-// circle.position = view.center;
-
-
-
-// var startArr = [];
-// function startPush(endpoint) {
-//     console.log("running...");
-//     console.log(startArr);  
-//     var newPoint = endpoint;
-//     do{
-//         console.log(startArr);
-//         console.log(newPoint);
-//         var newPoint = [Math.round(newPoint[0] - xDiff), Math.round(newPoint[1] - yDiff)];  
-//         console.log(newPoint);
-//         startArr.splice(0, 0, newPoint);
-//         console.log(startArr); } while (newPoint[0] > 0 && newPoint[1] > 0) 
-
-//     startArr.splice(0, 0, [0, 0]);
-//     console.log(startArr);
-// }
-
-
-// function onMouseUp(event) {
-
-//     var startX = myPath.firstSegment.point.x;
-//     var startY = myPath.firstSegment.point.y;
-//     var endX = myPath.lastSegment.point.x;
-//     var endY = myPath.lastSegment.point.y;
-//     var xDirection = endX - startX;
-//     var yDirection = endY - startY;
-//     console.log(xDirection);
-//     console.log(yDirection);
-//     console.log(endY);
-//     console.log(startY);
-//     var firstX = 100;
-//     var firstY = 100;
-//     var lastX = 0;
-//     var lastY = height;
-
-//     startPush([startX, startY]);
-
-//     var startPoints = [];
-
-//     for (var i = 0; i < startArr.length; i++) {
-//         startPoints[i] = new Point(startArr[i])
-//     }
-
-//     console.log(startPoints);
-//     console.log(myPath.segments);
-
-
-//     myPath.insertSegments(0, startPoints);
-//     myPath.add(new Point(lastX, lastY));
-    
-
-
-//     result = square.subtract(myPath);
-//     result.position = view.center;
-    
-
-//     // Mask the image:
-//     var group = new Group({
-//         children: [result, raster2],
-//         clipped: true
-//     });
-
-
-// }
 
 
 
